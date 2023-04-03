59c451173be227a799e11b69adb0804fa3f5f9c7
jameshay <james.ed.hay@gmail.com>
2023-04-03 23:52:18 +0100

    added UI elements through xcode and linked through to visual studio. Updated minimum system version in info.plist to stop error when running

--- NoteTaker/ViewController.designer.cs
+++ NoteTaker/ViewController.designer.cs
@@ -1,19 +1,44 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace NoteTaker
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		AppKit.NSScrollView previousNotes { get; set; }
+
+		[Action ("deleteBtn:")]
+		partial void deleteBtn (AppKit.NSButton sender);
+
+		[Action ("loadBtn:")]
+		partial void loadBtn (AppKit.NSButton sender);
+
+		[Action ("LoadBtn:")]
+		partial void LoadBtn (AppKit.NSButton sender);
+
+		[Action ("newBtn:")]
+		partial void newBtn (AppKit.NSButton sender);
+
+		[Action ("newNoteBtn:")]
+		partial void newNoteBtn (AppKit.NSButton sender);
+
+		[Action ("saveBtn:")]
+		partial void saveBtn (AppKit.NSButton sender);
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (previousNotes != null) {
+				previousNotes.Dispose ();
+				previousNotes = null;
+			}
 		}
 	}
 }
-
